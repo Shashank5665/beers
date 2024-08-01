c819faa3451289228a1c9a6c721ed135c887da9d
shashlad7 <shashankkr@ganakalabs.com>
2024-08-01 16:52:38 +0530

    project setup and Beerlist component

---
+++ src/Beerlist.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {
+  Container,
+  Grid,
+  Card,
+  CardMedia,
+  CardContent,
+  Typography,
+} from "@mui/material";
+
+const Beerlist = () => {
+  const [beers, setBeers] = useState([]);
+
+  useEffect(() => {
+    const fetchBeers = async () => {
+      try {
+        const response = await axios.get(
+          "https://api.sampleapis.com/beers/ale"
+        );
+
+        const sortedBeers = response.data.sort((beer) => {
+          const beerPrice = beer.price.replace("$", "");
+          return beerPrice;
+        });
+
+        const filteredBeers = sortedBeers.filter(
+          (beer) => beer.rating.average > 4.5
+        );
+
+        setBeers(filteredBeers);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchBeers();
+  }, []);
+
+  return (
+    <Container>
+      <Grid container spacing={3} m={1}>
+        {beers.map((beer) => (
+          <Grid item xs={12} sm={6} md={4} key={beer?.id}>
+            <Card>
+              <CardMedia
+                component="img"
+                height="200"
+                image={beer?.image}
+                alt={beer?.name}
+              />
+              <CardContent>
+                <Typography variant="h6">{beer?.name}</Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Price: {beer?.price}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Rating: {beer?.rating.average} ({beer?.rating.reviews}{" "}
+                  reviews)
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
+  );
+};
+
+export default Beerlist;
